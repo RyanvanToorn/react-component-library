412d439329dbc47ffe20801030bf5bf63589f06d
DarkLineRSA <ryanvantoorn@gmail.com>
2025-06-09 18:40:38 +0200

    Further development on Popup component

--- Popup/Popup.jsx
+++ Popup/Popup.jsx
@@ -1,13 +1,73 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Popup.module.css";
 
-function Popup(title = "Popup", subtitle = "", showCloseButton, isVisible = true, extendedClass = "", inlineStyles = {}) {
-  const [isOpen, setIsOpen] = useState(false);
-  if (!isVisible || !isOpen) {
+function Popup({
+  isOpen = false,
+  title = "",
+  subtitle = "",
+  bodyContents = null,
+  footerContents = null,
+  showCloseButton = true,
+  isVisible = true,
+  extendedClass = "",
+  inlineStyles = {},
+}) {
+  const [isPopupOpen, setIsOpen] = useState(isOpen);
+
+  useEffect(() => {
+    setIsOpen(isOpen);
+  }, [isOpen]);
+
+  if (!isVisible || !isPopupOpen) {
     return null;
   }
 
-  return <div className={`popup ${styles.Popup} ${extendedClass}`} style={inlineStyles}></div>;
+  function closePopup() {
+    setIsOpen(false);
+  }
+
+  return (
+    <div>
+      <div className={`popup-backdrop ${styles.PopupBackdrop}`}></div>
+      <div className={`popup ${styles.Popup} ${extendedClass}`} style={inlineStyles}>
+        <PopupHeader title={title} subtitle={subtitle} showCloseButton={showCloseButton} onClose={closePopup} />
+        <PopupContent bodyContents={bodyContents} />
+        <PopupFooter footerContents={footerContents} />
+      </div>
+    </div>
+  );
 }
+
+/* ===== Sub Components ===== */
+
+const PopupHeader = ({ title, subtitle, onClose, showCloseButton }) => {
+  return (
+    <div className={`popup-header ${styles.PopupHeader}`}>
+      <div className="popup-header__layout-container-left">
+        <div className="popup-header__title">{title}</div>
+        <div className="popup-header__subtitle">{subtitle}</div>
+      </div>
+      <div className="popup-header__layout-container-right">
+        <CloseButton onClose={onClose} />
+      </div>
+    </div>
+  );
+};
+
+const PopupContent = ({ bodyContents }) => {
+  return <div className={`popup-content ${styles.PopupContent}`}>{bodyContents}</div>;
+};
+
+const PopupFooter = ({ footerContents }) => {
+  return <div className={`popup-footer ${styles.PopupFooter}`}>{footerContents}</div>;
+};
+
+const CloseButton = ({ onClose }) => {
+  return (
+    <button className={`popup-close-button ${styles.CloseButton}`} onClick={onClose}>
+      X
+    </button>
+  );
+};
 
 export default Popup;
